9073cf737b1fe2f43ebd11e23dd776d8d161d51b
Pong <samfunghp@gmail.com>
2019-04-15 22:16:07 +0800

    make it better

--- scripts/component.js
+++ scripts/component.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const componentName = process.argv[2].replace(/^\w/, function(chr) {
-  return chr.toUpperCase();
-});
-const dir = path.join(__dirname, `../src/components/${componentName}`);
+const componentName = process.argv
+  .slice(2)
+  .find(v => !/-/.test(v))
+  .replace(/^\w/, function(chr) {
+    return chr.toUpperCase();
+  });
+const componentOnly = process.argv.find(v => v === '-s');
+const dir = path.join(__dirname, `../src/components/`, componentOnly ? '' : componentName);
 
 const write = (path, content) => {
   fs.writeFileSync(path, content.replace(/^ {2}/gm, '').replace(/^ *\n/, ''), 'utf-8');
